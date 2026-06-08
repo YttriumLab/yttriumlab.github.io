9414ae66a5c2a8578b239c7c3a2a8f6ee1d549d9
YttriumLab <45127+1@seijogakko.ed.jp>
2026-06-09 08:33:28 +0900

    Update SetMenu.js

--- js/SetMenu.js
+++ js/SetMenu.js
@@ -9,24 +9,41 @@
 (function()
 {
 	var retry=0;
+	var MAX_RETRY=1;
+	var url='/templates/menu.htm';
 	
 	var Load=function()
 	{
+		console.log('[Menu] Loading menu ... (try '+(retry+1)+')');
+		
 		var xhr=new XMLHttpRequest();
-		xhr.open('GET','/templates/menu.htm',true);
+		xhr.open('GET',url,true);
 		xhr.onreadystatechange=function()
 		{
 			if(xhr.readyState===4)
 			{
 				if(xhr.status===200)
-					document.getElementById('nav').innerHTML=xhr.responseText;
-				else if(retry<1)
 				{
+					document.getElementById('nav').innerHTML=xhr.responseText;
+					console.log('[Menu] Menu loaded successfully.');
+				}
+				else if(retry<MAX_RETRY)
+				{
+					console.warn('[Menu] Load failed (status: '+xhr.status+'). Retrying ... (try '+(retry+1)+')');
 					retry++;
-					window.setTimeout(Load,500);//retry on failure
+					console.log('[Menu] Retry scheduled in 500ms ...');
+					window.setTimeout(function()
+						{
+							console.log('[Menu] Retry started (try '+retry+')');
+							Load();
+						}
+						,500);//retry on failure
 				}
 				else
+				{
 					document.getElementById('nav').innerHTML='<div class="par">Menu unavailable.</div>';
+					console.error('[Menu] Final failure. URL: '+url+', status: '+xhr.status);
+				}
 			}
 		}
 		xhr.send();
